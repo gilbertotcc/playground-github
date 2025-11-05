4f4ead023e72c1483a678e8594eaad625f9d6896
Gilberto Taccari <9193796+gilbertotcc@users.noreply.github.com>
2025-11-05 22:45:48 +0100

    Add I/O functions (#20)
    Add functions to read PR URLs from file and to write a CSV report.

--- src/generate_report.py
+++ src/generate_report.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 
 from playgroundgithub.client.GitHubClient import Configuration, create_github_client
-from playgroundgithub.domain.PullRequestUrl import pull_request_from_url
-from playgroundgithub.service.AnalysisReporter import AnalysisReporter
 from playgroundgithub.service.PullRequestAnalyzer import PullRequestAnalyzer
+from playgroundgithub.utils.reader import load_pull_requests_from_file
+from playgroundgithub.utils.writer import csv_repport_of
 
 load_dotenv()
 
@@ -13,19 +13,14 @@ load_dotenv()
 def main() -> None:
     configuration = Configuration(os.getenv("GITHUB_TOKEN") or "")
     client = create_github_client(configuration)
-    
-    pull_request_urls = [
-        pull_request_from_url("https://github.com/totmoney/onboarding/pull/1989"),
-        pull_request_from_url("https://github.com/totmoney/docs-parser/pull/1"),
-        pull_request_from_url("https://github.com/totmoney/transactions/pull/2099"),
-    ]
+
+    pull_request_urls = load_pull_requests_from_file("develop/prs.txt")
 
     analyzer = PullRequestAnalyzer(client)
     metrics = analyzer.analyze_pull_requests(pull_request_urls)
-    
-    reporter = AnalysisReporter()
-    report = reporter.create_report(metrics)
-    
+
+    report = csv_repport_of(metrics)
+
     print(report)
 
     client.close()
--- src/playgroundgithub/client/GitHubClient.py
+++ src/playgroundgithub/client/GitHubClient.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from github import Auth, Github
+from github.IssueComment import IssueComment as GithubIssueComment
 from github.PullRequest import PullRequest as GithubPullRequest
 from github.PullRequestComment import PullRequestComment as GithubPullRequestComment
 
@@ -38,8 +39,8 @@ class GitHubClient:
         return self._to_pull_request(pull_request, pull_request_url)
 
     def get_pr_comments(
-            self, pull_request_url: PullRequestUrl
-        ) -> list[PullRequestComment]:
+        self, pull_request_url: PullRequestUrl
+    ) -> list[PullRequestComment]:
         """
         Gets the comments of a pull request.
 
@@ -47,8 +48,17 @@ class GitHubClient:
         :return: The comments of the pull request.
         """
         pull_request = self._get_pull_request(pull_request_url)
-        comments = pull_request.get_review_comments()
-        return [self._to_comment(comment) for comment in comments]
+        review_comments = pull_request.get_review_comments()
+
+        issue = self.client.get_repo(
+            f"{pull_request_url.owner}/{pull_request_url.repository}"
+        ).get_issue(pull_request_url.number)
+        issue_comments = issue.get_comments()
+
+        comments = [self._to_comment(comment) for comment in review_comments]
+        comments.extend([self._to_comment(comment) for comment in issue_comments])
+
+        return comments
 
     def close(self) -> None:
         """
@@ -73,7 +83,9 @@ class GitHubClient:
         )
 
     @staticmethod
-    def _to_comment(pr_comment: GithubPullRequestComment) -> PullRequestComment:
+    def _to_comment(
+        pr_comment: GithubPullRequestComment | GithubIssueComment
+    ) -> PullRequestComment:
         return PullRequestComment(
             user=User(pr_comment.user.login),
             url=pr_comment.html_url,
