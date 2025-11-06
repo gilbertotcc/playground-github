df20ae5f1c09370ff2aee7e338bf4f414bf598bf
Gilberto Taccari <gilberto.taccari@gmail.com>
2025-11-06 23:52:51 +0100

    Add `type` property to `User`
    Add the property `type` to the class `User` to allow to filter out the
    non-human users while creating reports.

--- src/playgroundgithub/domain/PullRequestAnalysis.py
+++ src/playgroundgithub/domain/PullRequestAnalysis.py
@@ -8,9 +8,10 @@ from playgroundgithub.domain.User import User
 @dataclass
 class PullRequestAnalysis:
     pull_request: PullRequest
-    user_comment_counts: dict[User, int] = field(default_factory=dict)
+    human_comment_counts: dict[User, int] = field(default_factory=dict)
 
     def add_comment(self, comment: PullRequestComment) -> None:
-        self.user_comment_counts[comment.user] = (
-            self.user_comment_counts.get(comment.user, 0) + 1
-        )
+        if comment.user.type == "User":  # Add only comments made by humans
+            self.human_comment_counts[comment.user] = (
+                self.human_comment_counts.get(comment.user, 0) + 1
+            )
--- src/playgroundgithub/utils/writer.py
+++ src/playgroundgithub/utils/writer.py
@@ -8,7 +8,7 @@ from playgroundgithub.domain.User import User
 def csv_repport_of(pull_request_analysis: list[PullRequestAnalysis]) -> str:
     unique_participants: set[User] = set()
     for pr_analysis in pull_request_analysis:
-        unique_participants.update(pr_analysis.user_comment_counts.keys())
+        unique_participants.update(pr_analysis.human_comment_counts.keys())
 
     sorted_participants = sorted(list(unique_participants), key=lambda u: u.name)
 
@@ -22,7 +22,7 @@ def csv_repport_of(pull_request_analysis: list[PullRequestAnalysis]) -> str:
 
     rows = [header]
     for pr_analysis in pull_request_analysis:
-        comments_count = sum(pr_analysis.user_comment_counts.values())
+        comments_count = sum(pr_analysis.human_comment_counts.values())
         row = [
             str(pr_analysis.pull_request.url.url),
             str(pr_analysis.pull_request.title),
@@ -32,7 +32,7 @@ def csv_repport_of(pull_request_analysis: list[PullRequestAnalysis]) -> str:
         ]
         row.extend(
             [
-                str(pr_analysis.user_comment_counts.get(participant, 0))
+                str(pr_analysis.human_comment_counts.get(participant, 0))
                 for participant in sorted_participants
             ]
         )
