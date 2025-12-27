c89e558badb765d2ba8c0e161db682b874abd188
Gilberto Taccari <gilberto.taccari@gmail.com>
2025-12-27 12:48:11 +0100

    Remove unused code

--- src/playgroundgithub/domain/__init__.py
+++ src/playgroundgithub/domain/__init__.py
@@ -1,5 +1,4 @@
 from .pull_requests import PullRequest as PullRequest
 from .pull_requests import PullRequestComment as PullRequestComment
 from .pull_requests import PullRequestUrl as PullRequestUrl
-from .reports import PullRequestAnalysis as PullRequestAnalysis
 from .user import User as User
